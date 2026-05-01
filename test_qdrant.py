0b41a458cfcfb6197edf7add5f2c4761d3093cf1
Kishore Reddy <kishorereddy5468@gmail.com>
2026-05-01 23:53:54 +0530

    chore: Fixed issue in test_qdrant.py file

--- test_qdrant.py
+++ test_qdrant.py
@@ -11,15 +11,3 @@ payload = {
     ]
 }
 
-r = httpx.put(
-    f"{settings.QDRANT_URL}/collections/stripe_docs/points",
-    headers={
-        "api-key": settings.QDRANT_API_KEY,
-        "Content-Type": "application/json",
-    },
-    json=payload,
-    timeout=60,
-)
-
-print(r.status_code)
-print(r.text)
